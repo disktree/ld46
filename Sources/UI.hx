33e5ca2c72753bcfaaf46207e0150cdbdb29304b
tong <tong@disktree.net>
2020-04-17 21:07:52 +0200

    optional custom ui themes

--- Sources/UI.hx
+++ Sources/UI.hx
@@ -6,8 +6,8 @@ class UI {
 
 	public static final FONT = "helvetica_neue_75.ttf";
 
-	public static final THEME = {
-		NAME: "Custom Dark",
+	public static final THEME_DEFAULT : TTheme = {
+		NAME: "Default Theme",
 		WINDOW_BG_COL: 0xff333333,
 		WINDOW_TINT_COL: 0xffffffff,
 		ACCENT_COL: 0xff444444,
@@ -40,13 +40,48 @@ class UI {
 		//LINK_STYLE: Line,
 	};
 
-	public static function create( callback : Zui->Void ) {
+	public static final THEME_BOOT : TTheme = {
+		NAME: "Boot Theme Dark",
+		WINDOW_BG_COL: 0xFF000000,
+		WINDOW_TINT_COL: 0xffffffff,
+		ACCENT_COL: 0xff444444,
+		ACCENT_HOVER_COL: 0xff494949,
+		ACCENT_SELECT_COL: 0xff606060,
+		BUTTON_COL: 0xff464646,
+		BUTTON_TEXT_COL: 0xffe8e7e5,
+		BUTTON_HOVER_COL: 0xff494949,
+		BUTTON_PRESSED_COL: 0xff1b1b1b,
+		TEXT_COL: 0xffe8e7e5,
+		LABEL_COL: 0xffc8c8c8,
+		SEPARATOR_COL: 0xff272727,
+		HIGHLIGHT_COL: 0xff205d9c,
+		CONTEXT_COL: 0xff222222,
+		PANEL_BG_COL: 0xff3b3b3b,
+		FONT_SIZE: 32,
+		ELEMENT_W: 100,
+		ELEMENT_H: 48,
+		ELEMENT_OFFSET: 4,
+		ARROW_SIZE: 5,
+		BUTTON_H: 22,
+		CHECK_SIZE: 15,
+		CHECK_SELECT_SIZE: 8,
+		SCROLL_W: 6,
+		TEXT_OFFSET: 8,
+		TAB_W: 6,
+		FILL_WINDOW_BG: false,
+		FILL_BUTTON_BG: true,
+		FILL_ACCENT_BG: false,
+		//LINK_STYLE: Line,
+	};
+
+	public static function create( ?theme : TTheme, callback : Zui->Void ) {
+		if( theme == null ) theme = THEME_DEFAULT;
 		Data.getFont( FONT, function(f:kha.Font) {
 			var ui = new Zui( {
 				font: f,
-				//theme: App.UI_THEME // now working
+				theme: theme // now working
 			} );
-			ui.ops.theme = THEME;
+			ui.ops.theme = theme;
 			callback( ui );
 		});
 	}
--- Sources/ld46/scene/Boot.hx
+++ Sources/ld46/scene/Boot.hx
@@ -1,32 +1,17 @@
 package ld46.scene;
 
+import zui.Id;
+import zui.Themes;
+import zui.Zui;
+
 class Boot extends Trait {
 
+	var ui : Zui;
 	var timer : TAnim;
 
 	public function new() {
 		super();
-		notifyOnInit( () -> {
-			
-			//#if !dev
-			Data.getSound( 'boot.wav', (s:kha.Sound) -> {
-				var channel = Audio.play( s, false, true );
-				channel.volume = 0.7;
-			});
-			//#end
-
-			timer = Tween.timer( 1, () -> {
-				resume();
-			});
-
-			notifyOnUpdate( () -> {
-				var keyboard = Input.getKeyboard();
-				var mouse = Input.getMouse();
-				if( keyboard.started('*') || mouse.started() ) {
-					resume();
-				}
-			} );
-		} );
+		notifyOnInit( init );
 	}
 
 	function init() {
@@ -35,31 +20,52 @@ class Boot extends Trait {
 
 		Scene.active.camera = Scene.active.getCamera( 'Camera_Boot' );
 
+		UI.create( UI.THEME_BOOT, ui -> this.ui = ui );
+
+		//#if !dev
 		Data.getSound( 'boot.wav', (s:kha.Sound) -> {
 			var channel = Audio.play( s, false, true );
 			channel.volume = 0.7;
 		});
+		//#end
 
 		timer = Tween.timer( 2, () -> {
 			resume();
 		});
 
-		///var cameraObject = scene.getCamera( 'BootCamera' );
-		//trace(cameraObject);
-		//trace("PLAY MUSIC");
-		//App.music.play("Machine-QuiteRumble");
-		//App.music.play("boot");
+		notifyOnUpdate( update );
+		notifyOnRender2D( render2D );
 	}
 
 	function update() {
-		/*
 		var keyboard = Input.getKeyboard();
 		var mouse = Input.getMouse();
-		if( keyboard.started( "enter" ) || keyboard.started( "space" )
-			|| mouse.started() ) {
-				resume();
+		if( keyboard.started('*') || mouse.started() ) {
+			resume();
+		}
+	}
+
+	function render2D( g : kha.graphics2.Graphics ) {
+		g.end();
+		if( ui != null ) {
+			ui.begin( g );
+			if( ui.window( Id.handle(), 20, 20, 240, 600, false ) ) {
+				ui.text("BOOT", Center );
+				/*
+				if( ui.panel( Id.handle( { selected: true } ), "Info" ) ) {
+					ui.indent();
+					ui.text("LD46");
+					ui.text("DISKTREE.NET");
+					if( ui.button( "PLAY" ) ) {
+						loadScene( 'Game' );
+					}
+					ui.unindent();
+				}
+				*/
 			}
-			*/
+			ui.end();
+		}
+        g.begin( false );
 	}
 
 	inline function resume() {
@@ -67,18 +73,6 @@ class Boot extends Trait {
 			Tween.stop( timer );
 			timer = null;
 		}
-		//App.music.stop();
-		//App.music.play("Machine-QuiteRumble");
-		//App.music.play("Bass-Deep 05");
-		//trace("resume");
-		//trace(App.music.volume);
-		//App.music.volume = 0.05;
-		///trace(App.music.volume);
-		/*
-		App.music.stop( () -> {
-			Scene.setActive( 'Menu' );
-		});
-		*/
 		Scene.setActive( 'Mainmenu' );
 	}
 
