7a9ffd03167e6995a34f2e762d23ade220f11991
tong <tong@disktree.net>
2020-04-18 00:19:45 +0200

    use krom internal storage to load/save game state

--- Sources/ld46/scene/Game.hx
+++ Sources/ld46/scene/Game.hx
@@ -1,21 +1,28 @@
 package ld46.scene;
 
 import armory.trait.internal.CanvasScript;
+import iron.system.Storage;
 import zui.*;
 
 typedef State = {
-	var time : Float;
+	var count : Int;
+	//var time : Float;
 }
 
 class Game extends Trait {
 
 	#if kha_krom
-	static final STATE_FILE = Krom.getFilesLocation()+'/state.json';
+	//static final STATE_FILE = Krom.getFilesLocation()+'/state.json';
 	#end
+
+	var monkey : MeshObject;
 
 	public function new() {
 		super();
 		notifyOnInit( init );
+		notifyOnRemove( () -> {
+			saveState();
+		} );
 	}
 	
 	function init() {
@@ -24,11 +31,12 @@ class Game extends Trait {
 		
 		Scene.active.camera = Scene.active.getCamera( 'Camera_Game' );
 		
-		loadState();
+		var state = loadState();
+		trace(state); //TODO
+
+		monkey = Scene.active.getMesh('Suzanne');
 		
 		notifyOnUpdate( update );
-
-		saveState();
 	}
 
 	function update() {
@@ -40,17 +48,25 @@ class Game extends Trait {
 		if( keyboard.started( "escape" ) ) {
 			Scene.setActive( "Mainmenu" );
 		}
+
+		monkey.transform.rotate( Vec4.zAxis(), 0.01 );
 	}
 	
 	function saveState() {
-		#if kha_krom
+		var count = Storage.data.count;
+		count = (count == null) ? 0 : count+1;
+		Storage.data = { count: count };
+		Storage.save();
+		/* #if kha_krom
 		var state = { time: 23 }; //TODO
 		var bytes = Bytes.ofString( Json.stringify( state ) );
 		Krom.fileSaveBytes( STATE_FILE, bytes.getData() );
-		#end
+		#end */
 	}
-
+	
 	function loadState() {
+		return Storage.data;
+		/*
 		#if kha_krom
 		Data.getBlob( STATE_FILE, b -> {
 			var state = Json.parse( b.toString() );
@@ -58,6 +74,7 @@ class Game extends Trait {
 			trace(state);
 		});
 		#end
+		*/
 	}
 	
 }
