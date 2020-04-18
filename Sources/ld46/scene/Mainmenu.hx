c819130661866a37164795244a4a4013f183c199
tong <tong@disktree.net>
2020-04-18 14:39:17 +0200

    remove settings menu options

--- Sources/ld46/scene/Mainmenu.hx
+++ Sources/ld46/scene/Mainmenu.hx
@@ -14,7 +14,6 @@ class Mainmenu extends Trait {
 		[1280,720],
 	];
 
-	//var canvas : CanvasScript;
 	var ui : Zui;
 	var audio : AudioChannel;
 	var currentScreenResolution : Int;
@@ -81,13 +80,13 @@ class Mainmenu extends Trait {
 	function render2D( g : kha.graphics2.Graphics ) {
 		g.end();
 		if( ui != null ) {
-			var cfg = Config.raw;
 			ui.begin( g );
+			
 			if( ui.window( Id.handle(), 20, 20, 300, 600, false ) ) {
-				//ui.text("LD46");
-				//ui.text("DISKTREE.NET");
 				if( ui.panel( Id.handle( { selected: true } ), "SETTINGS" ) ) {
 					//var resolution = ui.combo( Id.handle( { position: currentScreenResolution } ), [for(r in SCREEN_RESOLUTION) r[0]+'x'+r[1]], 'RESOLUTION' );
+					/*
+					var cfg = Config.raw;
 					var bloom = ui.check( Id.handle( { selected: cfg.rp_bloom } ), 'BLOOM' );
 					var dynres = ui.check( Id.handle( { selected: cfg.rp_dynres } ), 'DYNAMIC RESOLUTIOM' );
 					var motionblur = ui.check( Id.handle( { selected: cfg.rp_motionblur } ), 'MOTIONBLUR' );
@@ -100,10 +99,11 @@ class Mainmenu extends Trait {
 						var res = SCREEN_RESOLUTION[resolution];
 						cfg.window_w = res[0]; 
 						cfg.window_h = res[1];
-						*/
+						* /
 						RenderPathCreator.applyConfig();
 						Config.save();
 					}
+					*/
 					if( ui.button( 'PLAY' ) ) {
 						loadScene( 'Game' );
 					}
